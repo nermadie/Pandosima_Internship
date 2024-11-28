b8c13b1ada4b1e76eb96fe2843a60c93c6f0018a
MinhTran <nhatminh10b1@gmail.com>
2024-11-28 15:27:27 +0700

    tutorial-part-2 28.11.2024

--- 1.Tutorial/tutorial_project/tutorial_project/settings.py
+++ 1.Tutorial/tutorial_project/tutorial_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig', # Add the polls app to the INSTALLED_APPS setting
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
